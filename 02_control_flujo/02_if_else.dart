b8f37584065f5e1c690b92709321f46ce0681e11
Héctor Alvarez <halvarezr@outlook.com>
2022-07-18 20:58:03 -0500

    for

--- 02_control_flujo/02_if_else.dart
+++ 02_control_flujo/02_if_else.dart
@@ -21,4 +21,16 @@ main() {
   // } else {
   //   stdout.writeln('Eres menor de edad');
   // }
+
+  /* EJERCICIO
+    Crear un programa en dart que:
+      Si eres mayor o igual a 21 años, mostrar la palabra "ciudadano"
+      Si estás entre 18 y 20 (incluyendo 18), mostrar "Mayor de edad"
+      Si eres menor a 18 (sin contar 18), mostrar "menor de edad"
+      28 ciudadano
+      21 ciudadano
+      20 mayor de edad
+      18 mayor de edad
+      17 menor de edad
+  */
 }
---
+++ 02_control_flujo/03_for.dart
@@ -0,0 +1,17 @@
+void main() {
+  // for( int i = 1; i <= 10; i++ ) {
+  //   print('index i:  ${  2 * 10  }');
+  // }
+
+  /* EJERCICIO
+       Dato de entrada: La base de la tabla de multiplicar
+       (este dato debe de ser capturado por el usuario)
+       ej: 2      2, 4, 6, 8, 10
+       La salida esperada sería
+       2 * 1 = 2
+       2 * 2 = 4
+       2 * 3 = 6
+       ..
+       2 * 10 = 20
+  */
+}
