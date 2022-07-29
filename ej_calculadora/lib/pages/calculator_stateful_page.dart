e5a878c7244f525037ba4f134aede98068c5460f
Héctor Alvarez <halvarezr@outlook.com>
2022-07-28 19:59:48 -0500

    init

---
+++ ej_calculadora/lib/pages/calculator_stateful_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CalculatorStatefulPage extends StatefulWidget {
+  CalculatorStatefulPage({Key? key}) : super(key: key);
+
+  @override
+  State<CalculatorStatefulPage> createState() => _CalculatorStatefulPageState();
+}
+
+class _CalculatorStatefulPageState extends State<CalculatorStatefulPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
